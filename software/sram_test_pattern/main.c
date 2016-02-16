21746aa8d21ee39b703cc9d2d310968d96ab35b3
Stefan <smartynk@ualberta.ca>
2016-02-16 09:58:12 -0700

    Added new test code

--- software/sram_test_pattern/main.c
+++ software/sram_test_pattern/main.c
@@ -11,22 +11,98 @@
 #include <io.h>
 #include <system.h>
 #include <sys/alt_stdio.h>
+#include <string.h>
+
+
+void printPalette(int n){
+	// Print everything in the palette ram, upto int colours.
+	int i;
+	unsigned int c;
+
+	unsigned int results[512] = {'\0'};
+
+	for (i = 0; i < n; i++){
+		c = IORD_16DIRECT(COLOUR_PALETTE_SHIFTER_0_BASE, 2*i); //offset multiplied by 2 to be on 16-bit boundaries.
+		//alt_printf("palette[ %x ]: %x ", 2*i, c);
+		results[i] = c;
+	}
+
+	for (i = 0; i < n; i++){
+		alt_printf("palette[ %x ]: %x ", 2*i, results[i]);
+	}
+
+}
+
+
+//Setting address 0xFF will change the border colour.
+void changeBorderColor(unsigned int col){
+
+	IOWR_16DIRECT(COLOUR_PALETTE_SHIFTER_0_BASE, 2*0xFF, col);
+
+
+}
+
 
 int main()
 { 
 	int row = 0;
 	int col = 0;
 	int color;
-	alt_putstr("Starting write\n");
+
+	unsigned int i = 0;
+	unsigned int delay = 0;
+	// Clear the screen first.
+	alt_putstr("Clearing Screen\n");
+
+
+	for (col = 0; col < 640; col = col + 4){
+		for (row = 0; row < 480; row++){
+			color = 0;
+			IOWR_32DIRECT(SRAM_0_BASE, row * 640 + col, color << 24 | color << 16 | color << 8 | color << 0);
+		}
+	}
+
+	changeBorderColor(0xF800);
+
+//	for (i = 0; i <= 16; i++){
+//		alt_printf("Colour: %u", i);
+//		for (delay = 0; delay < 2000; delay++){
+//			unsigned int tdelay = delay;
+//			for (tdelay; tdelay > 0; tdelay--){}
+//		}
+//
+//		for (row=0; row<480; row++){
+//			for (col = 0; col < 640; col=col+4){
+//				color = i;
+//				IOWR_32DIRECT(SRAM_0_BASE, row * 640 + col, color << 24 | color << 16 | color << 8 | color << 0);
+//			}
+//
+//		}
+//	}
+
+	printPalette(7);
+
+	alt_putstr("\nStarting write\n");
 
 	for (row = 0; row < 480; row++)
 	{
+
+//		if (row%2 == 1){
+//			//Skip every 4th row for clarity? Keeping the first one and last one.
+//			continue;
+//		}
+
 		for (col = 0; col < 640; col = col + 4)
 		{
 			color = ((row + col) % 256) << 0 | ((row + col) % 256) << 8 | ((row + col) % 256) << 16 | ((row + col) % 256) << 24;
 			//color = 3;
 
-			IOWR_32DIRECT(SRAM_0_BASE, row * 640 + col, color << 24 | color << 16 | color << 8 | color << 0);
+			//color = 0; // Dark blue is first on colour band.
+
+			//Want to get a bunch of 1px by 1px colour blocks. Also want the colours to keep incrementing and wrapping around.
+			//color = (color + col)%15; //max val of colour is 14, which should be yellow.
+
+			//IOWR_32DIRECT(SRAM_0_BASE, row * 640 + col, (color+3) << 24 | (color+2) << 16 | (color+1) << 8 | (color+0) << 0);
 			if (row == 0 || row == 479)
 			{
 				IOWR_32DIRECT(SRAM_0_BASE, row * 640 + col, 0xFFFFFFFF);
