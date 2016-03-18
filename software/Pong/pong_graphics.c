77b6a6f50a7991843e7359413a292ad15a326eb6
Mason Strong <mstrong@ualberta.ca>
2016-03-17 21:04:35 -0600

    all the numbers, 6 needs to be fixed

--- software/Pong/pong_graphics.c
+++ software/Pong/pong_graphics.c
@@ -3,8 +3,14 @@
 
 #include "graphics_commands.h"
 
+int scale_input(int x){
+	return x / 10;
+}
+
 void draw_ball(int x, int y)
 {
+	x = scale_input(x);
+	y = scale_input(y);
 	draw_rectangle(x-1, y+3, x+1, y+3, 0xFF);
 	draw_rectangle(x-2, y+2, x+2, y+2, 0xFF);
 	draw_rectangle(x-3, y-1, x+3, y+1, 0xFF);
@@ -14,6 +20,8 @@ void draw_ball(int x, int y)
 
 void draw_paddle(int x, int y)
 {
+	x = scale_input(x);
+	y = scale_input(y);
 	draw_rectangle(x-5, y-18, x+5, y+18, 0xFF);
 }
 
@@ -32,6 +40,9 @@ void draw_field()
 }
 
 void draw_int(int x, int y, int value, int color){
+	/*
+	 * Fonts based on https://saelaenx.files.wordpress.com/2012/10/emon-font-size-04-01.png
+	 */
 	if (value > 9 || value < 0)
 		return;
 	else if (value == 0){
@@ -39,31 +50,154 @@ void draw_int(int x, int y, int value, int color){
 		draw_rectangle( (x+1), (y+1), (x+13), (y+1), color);
 		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
 		draw_rectangle( (x+0), (y+4), (x+3), (y+23), color);
-		draw_rectangle( (x+11), (y+4), (x+14), (y+23), color);
-		draw_rectangle( (x+0), (y+24), (x+14), (y+25), color);
-		draw_rectangle( (x+1), (y+26), (x+13), (y+26), color);
-		draw_rectangle( (x+2), (y+27), (x+12), (y+27), color);
+		draw_rectangle( (x+11), (y+4), (x+14), (y+21), color);
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+1), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+2), (y+25), (x+12), (y+25), color);
 	}
 
 	else if (value == 1){
-		draw_rectangle( (x+1), (y+1), (x+1), (y+1), color);
-		draw_rectangle( (x+2), (y), (x+2), (y+4), color);
+		draw_rectangle( (x+2), (y+0), (x+8), (y+0), color);
+		draw_rectangle( (x+2), (y+1), (x+9), (y+1), color);
+		draw_rectangle( (x+2), (y+2), (x+10), (y+3), color);
+		draw_rectangle( (x+5), (y+4), (x+10), (y+21), color);
+		draw_rectangle( (x+0), (y+22), (x+14), (y+25), color);
 	}
 
 	else if (value == 2){
-		draw_rectangle( (x+0), (y+0), (x+3), (y+0), color);
-		draw_rectangle( (x+4), (y+1), (x+4), (y+1), color);
-		draw_rectangle( (x+1), (y+2), (x+3), (y+2), color);
-		draw_rectangle( (x+0), (y+3), (x+0), (y+4), color);
-		draw_rectangle( (x+1), (y+4), (x+4), (y+4), color);
+		draw_rectangle( (x+0), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+0), (y+1), (x+13), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+11), (y+4), (x+14), (y+10), color);
+
+		draw_rectangle( (x+2), (y+11), (x+14), (y+11), color);
+		draw_rectangle( (x+1), (y+12), (x+14), (y+12), color);
+		draw_rectangle( (x+0), (y+13), (x+13), (y+13), color);
+		draw_rectangle( (x+0), (y+14), (x+12), (y+14), color);
+
+		draw_rectangle( (x+0), (y+15), (x+3), (y+21), color);
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+1), (y+24), (x+14), (y+24), color);
+		draw_rectangle( (x+2), (y+25), (x+14), (y+25), color);
 	}
 
 	else if (value == 3){
-		draw_rectangle( (x+0), (y+0), (x+3), (y+0), color);
-		draw_rectangle( (x+4), (y+1), (x+4), (y+1), color);
-		draw_rectangle( (x+1), (y+2), (x+3), (y+2), color);
-		draw_rectangle( (x+4), (y+3), (x+4), (y+3), color);
-		draw_rectangle( (x+0), (y+4), (x+3), (y+4), color);
+		draw_rectangle( (x+0), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+0), (y+1), (x+13), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+11), (y+4), (x+14), (y+10), color);
+
+		draw_rectangle( (x+2), (y+11), (x+14), (y+14), color);
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+21), color);
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+0), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+0), (y+25), (x+12), (y+25), color);
+	}
+
+	else if (value == 4){
+		draw_rectangle( (x+0), (y+0), (x+3), (y+10), color);
+
+		draw_rectangle( (x+11), (y+0), (x+14), (y+10), color);
+
+		draw_rectangle( (x+0), (y+11), (x+14), (y+12), color);
+		draw_rectangle( (x+1), (y+13), (x+14), (y+13), color);
+		draw_rectangle( (x+2), (y+14), (x+14), (y+14), color);
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+25), color);
+	}
+
+	else if (value == 5){
+		draw_rectangle( (x+2), (y+0), (x+14), (y+0), color);
+		draw_rectangle( (x+1), (y+1), (x+14), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+0), (y+4), (x+3), (y+10), color);
+
+		draw_rectangle( (x+0), (y+11), (x+12), (y+11), color);
+		draw_rectangle( (x+0), (y+12), (x+13), (y+12), color);
+		draw_rectangle( (x+1), (y+13), (x+14), (y+13), color);
+		draw_rectangle( (x+2), (y+14), (x+14), (y+14), color);
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+21), color);
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+0), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+0), (y+25), (x+12), (y+25), color);
+	}
+
+	else if (value == 6){
+		draw_rectangle( (x+2), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+1), (y+1), (x+12), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+12), (y+3), color);
+
+		draw_rectangle( (x+0), (y+4), (x+3), (y+10), color);
+		//TODO: Slightly wrong on the loop, right hand side, too square I think
+		draw_rectangle( (x+0), (y+11), (x+13), (y+12), color);
+		draw_rectangle( (x+0), (y+13), (x+14), (y+13), color);
+		draw_rectangle( (x+0), (y+14), (x+14), (y+14), color);
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+21), color);
+
+		draw_rectangle( (x+0), (y+15), (x+3), (y+21), color);
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+1), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+2), (y+25), (x+12), (y+25), color);
+	}
+
+	else if (value == 7){
+		draw_rectangle( (x+0), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+0), (y+1), (x+13), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+11), (y+4), (x+14), (y+25), color);
+	}
+
+	else if (value == 8){
+		draw_rectangle( (x+2), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+1), (y+1), (x+13), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+0), (y+4), (x+3), (y+10), color);
+
+		draw_rectangle( (x+11), (y+4), (x+14), (y+10), color);
+
+		draw_rectangle( (x+0), (y+11), (x+14), (y+14), color);
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+21), color);
+
+		draw_rectangle( (x+0), (y+15), (x+3), (y+21), color);
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+1), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+2), (y+25), (x+12), (y+25), color);
+	}
+
+	else if (value == 9){
+		draw_rectangle( (x+2), (y+0), (x+12), (y+0), color);
+		draw_rectangle( (x+1), (y+1), (x+13), (y+1), color);
+		draw_rectangle( (x+0), (y+2), (x+14), (y+3), color);
+
+		draw_rectangle( (x+0), (y+4), (x+3), (y+10), color);
+
+		draw_rectangle( (x+11), (y+4), (x+14), (y+10), color);
+
+		draw_rectangle( (x+0), (y+11), (x+13), (y+14), color);
+		draw_rectangle( (x+1), (y+13), (x+14), (y+14), color);
+		draw_rectangle( (x+2), (y+14), (x+14), (y+14), color);
+
+
+		draw_rectangle( (x+11), (y+15), (x+14), (y+21), color);
+
+
+		draw_rectangle( (x+0), (y+22), (x+14), (y+23), color);
+		draw_rectangle( (x+1), (y+24), (x+13), (y+24), color);
+		draw_rectangle( (x+2), (y+25), (x+12), (y+25), color);
 	}
 
 	else
